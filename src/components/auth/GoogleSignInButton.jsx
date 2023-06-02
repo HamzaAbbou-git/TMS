d70940f3dcf6be21f25ff3c78e1ff4c931c178aa
Hamza ABBOU <habbou@MacBook-Pro-de-servicepc-2.local>
2023-06-02 11:10:28 +0200

    signup with google

--- src/components/auth/GoogleSignInButton.jsx
+++ src/components/auth/GoogleSignInButton.jsx
@@ -5,17 +5,32 @@ import '../style/SignIn.css';
 import image from '../../images/logoRodeo.png';
 import { FiUser, FiLock } from 'react-icons/fi';
 import { FaGoogle } from 'react-icons/fa';
+import { getAuth } from "firebase/auth";
 
 
 const GoogleSignInButton = () => {
   const handleSignIn = async () => {
     const provider = new GoogleAuthProvider();
-
-    try {
-      await signInWithPopup(provider);
-    } catch (error) {
-      console.error(error);
-    }
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
+    .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        // IdP data available using getAdditionalUserInfo(result)
+        // ...
+    }).catch((error) => {
+    // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+    });
   };
 
   return (
